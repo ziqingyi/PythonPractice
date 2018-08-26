771cbab204f6a0636c2e986b05a52781b66fcdd1
sun <sun@sunJianqiangdeMacBook-Air.local>
2018-08-26 18:33:00 +1000

    add test for tom

--- TomandGun.py
+++ TomandGun.py
@@ -4,12 +4,25 @@ class Person(object):
 	def __init__(self,name):
 		super(Person,self).__init__()
 		self.name = name
+		self.gun = None # reference to the gun
+		self.hp = 100
 
 	def load_bullet(self,clip_temp,bullet_temp):
 		clip_temp.add_bullet(bullet_temp)
 
 	def load_clip(self,gun_temp,clip_temp):
 		gun_temp.accept_clip(clip_temp)
+
+	def pickUpGun(self,gun_temp):
+		"""pick up a gun"""
+		self.gun = gun_temp
+
+	def __str__(self):
+		if self.gun:
+			return "%s has blood: %d, with gun %s"%(self.name,self.hp,self.gun)
+		else:
+			return "%s has blood: %d, no gun"%(self.name,self.hp,self.gun)
+
 
 
 class Gun(object):
@@ -23,7 +36,10 @@ class Gun(object):
 		self.clip = clip_temp
 
 	def __str__(self):
-		return "the info of gun: %s" %(self.name)
+		if self.clip:
+			return "the info of gun: %s, %s" %(self.name,self.clip)
+		else:
+			return "the info of gun: %s" %(self.name)
 
 
 class Clip(object):
@@ -73,6 +89,14 @@ def main():
 
     #test ak47
     print(ak47)
+
+    #7 tom use the gun
+    Tom.pickUpGun(ak47)
+
+    #test: test tom
+    print(Tom)
+
+
 
 
 if __name__ == '__main__':
