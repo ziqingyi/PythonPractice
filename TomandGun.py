3125e1dbf0c685aef3312fd0b02490212c5d1dbc
sun <sun@sunJianqiangdeMacBook-Air.local>
2018-08-26 18:04:03 +1000

    add test,fix bugs

--- TomandGun.py
+++ TomandGun.py
@@ -9,7 +9,7 @@ class Person(object):
 		clip_temp.add_bullet(bullet_temp)
 
 	def load_clip(self,gun_temp,clip_temp):
-		gun_temp.accept_clip(clip)
+		gun_temp.accept_clip(clip_temp)
 
 
 class Gun(object):
@@ -22,10 +22,13 @@ class Gun(object):
 	def accept_clip(self,clip_temp):
 		self.clip = clip_temp
 
+	def __str__(self):
+		return "the info of gun: %s" %(self.name)
+
 
 class Clip(object):
 
-	def __init_(self,max_num):
+	def __init__(self,max_num):
 		super (Clip,self).__init__()
 		self.max_num= max_num # max capacity
 		self.bullet_list = [] # store reference to B
@@ -33,13 +36,15 @@ class Clip(object):
 		""""add bullet to clip"""
 		self.bullet_list.append(bullet_temp)
 
+	def __str__(self):
+		return "the info of clip: %d/%d"%(len(self.bullet_list),self.max_num)  # ava/total
 
 
 
 class Bullet(object):
-	def __init__(self,power):
-		super(Bullet,self).init()
-		self,power = power # power of the bullet
+	def __init__(self, power):
+		super(Bullet, self).__init__()
+		self.power = power # power of the bullet
 
 
 def main():
@@ -53,21 +58,21 @@ def main():
     #3 create a Clip object
     clip_1 = Clip(20)
 
-    #4 create bullet
-    bullet_1 = Bullet(10)
+    #4 create bullets
+    for i in range(15):
+    	bullet_1 = Bullet(10)
 
     #5 Tom load the bullet to clip
-    Tom.load_bullet(clip_1,bullet_1)
+    	Tom.load_bullet(clip_1,bullet_1)
 
     #6 load the clip to the gun
     Tom.load_clip(ak47,clip_1)
 
+    # test: clip_1
+    print(clip_1)
 
-
-
-
-
-
+    #test ak47
+    print(ak47)
 
 
 if __name__ == '__main__':
