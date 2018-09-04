d897df34a6f9ed0efd2624cb8cc7a834d1238be4
sun <sun@192-168-1-53.tpgi.com.au>
2018-09-04 22:08:33 +1000

    add button check

--- PlaneGame.py
+++ PlaneGame.py
@@ -2,6 +2,7 @@
 
 import pygame
 import time
+from pygame.locals import *
 
 def main():
 
@@ -14,15 +15,40 @@ def main():
 	#3. create a plan picture
 	hero = pygame.image.load("./pic/hero1.png")
 
+	x = 210
+	y = 700
+
 	while True:
 
 		screen.blit(background,(0,0))
 
-		screen.blit(hero,(230,700))
+		screen.blit(hero,(x,y))
 
 		pygame.display.update()
 
-		#time.sleep(5)
+		#get the event, eg.Press button
+		for event in pygame.event.get():
+
+			#if press QUIT button
+			if event.type == QUIT:
+				print("exit")
+				exit()
+			#if press down button
+			elif event.type == KEYDOWN:
+				#if press a or left button
+				if event.key == K_a or event.key ==K_LEFT:
+					print('left')
+				#if press d or right button
+				elif event.key == K_d or event.key == K_RIGHT:
+					print('right')
+				#if press space 
+				elif event.key == K_SPACE:
+					print('space')
+
+
+
+
+		time.sleep(0.01)
 
 
 if __name__ == "__main__":
